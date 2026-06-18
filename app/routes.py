b07a4f6e9820f172cfcfaea2a4a9269a6df28b00
identity-fraud <william.kim22@education.nsw.gov.au>
2026-06-18 13:30:36 +1000

    login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
+from urllib.parse import urlsplit
 from app import app, db
 from app.forms import LoginForm
 from app.models import User
